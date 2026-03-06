db1dad3200d1decdba94a7ebe5e1052d62ec9dd5
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-03-06 14:43:07 -0300

    fix: code

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,12 +1,7 @@
-// O restante muda pois 
-//BASE DA URL = https://api.themoviedb.org/3
-//URL DA API = https://api.themoviedb.org/3/movie/now_playing?api_key=8da13dd16afa022057d1239915a47e59&language=pt-BR
-
-
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://api.themoviedb.org/3"
-})
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api.themoviedb.org/3"
+});
 
 export default api;
